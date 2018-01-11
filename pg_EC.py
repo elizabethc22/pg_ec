0dc7c8f3bbd05455610da709f1d1a8375201100c
elizabethc22 <33519368+elizabethc22@users.noreply.github.com>
2018-01-11 09:04:55 -0500

    Add files via upload

---
+++ pg_EC.py
@@ -0,0 +1,26 @@
+import pyautogui as pg
+import time
+pg.moveTo(1185, 34)
+time.sleep(3)
+pg.click(1185, 34, 1)
+pg.typewrite('Google Drive')
+pg.click(787, 64, 1)
+time.sleep(3)
+pg.click(1161, 287, 1)
+time.sleep(3)
+pg.click(1059, 497, 1)
+time.sleep(3)
+pg.click(928, 542, 1)
+time.sleep(3)
+pg.typewrite('ecutting@gcds.net')
+pg.click(1168, 473, 1)
+time.sleep(3)
+pg.typewrite(bl
+time.sleep(3)
+pg.click(1289, 431, 2)
+time.sleep(3)
+pg.click(1099, 508, 1)
+pg.typewrite('Hello Person, I am a computer and I am  editing your work!')
+
+
+
